5e7bc2642ec2a55f1283a21cb27c923d46f68587
Victor Emanoel <VituTrabalho@outlook.com>
2024-03-05 20:50:34 -0300

    ATT

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from modelos.restaurante import Restaurante
-
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 Restaurante_praca = Restaurante('praça', 'Gourmet')
-
-
-Restaurante_praca.receber_avaliacao('gui', 5)
-Restaurante_praca.receber_avaliacao('lais', 8)
-Restaurante_praca.receber_avaliacao ('victor', 2)
-
-
+bebida_suco = Bebida('Suco de melancia', 5.00, 'grande')
+bebida_suco.aplicar_desconto()
+prato_pão = Prato ('Pão', 2.00, 'Melhor pão da cidade')
+prato_pão.aplicar_desconto()
+Restaurante_praca.adicionar_no_cardapio(bebida_suco)
+Restaurante_praca.adicionar_no_cardapio(prato_pão)
 
 
 def main():
-    Restaurante.listar_restaurantes()
+    Restaurante_praca.exibir_cardapio
 
 
 if __name__ == '__main__':
